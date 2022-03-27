15a958bb157bd8d98b78b8bfcfbae9811c394910
SMITA MANE <SMITA MANE@LAPTOP-TQDILSNP>
2022-03-27 02:39:59 -0700

    Second commit

---
+++ M11-C2TC/src/javaapplication/SumNaturalNum.java
@@ -0,0 +1,16 @@
+package javaapplication;
+
+public class SumNaturalNum {
+
+	public static void main(String[] args) {
+		int number = 100;
+		int sum=0;
+		for(int i=1; i<=100; i++)
+		{
+			sum = sum +i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
