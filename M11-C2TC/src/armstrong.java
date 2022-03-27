e6d82319da92a10bda4934ce0848741c63f6ec23
SMITA MANE <SMITA MANE@LAPTOP-TQDILSNP>
2022-03-27 04:02:59 -0700

    Armstrong commit

--- M11-C2TC/src/armstrong.java
+++ M11-C2TC/src/armstrong.java
@@ -1,8 +1,27 @@
 
 public class armstrong {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] arg)
+			{
+			Scanner sc=new Scanner(System.in);
+			int number=sc.nextInt();
+			int temp=number;
+			int rem=0;
+			int sum=0;
+			while(number>0)
+			{
+				rem=number%10;
+				sum=sum+(rem*rem*rem);
+				number=number/10;
+			}
+				if(temp==sum)
+				{
+					System.out.println("Armstrong ");
+				}
+				else
+				{
+					System.out.println("Not Armstrong");
+
 
 	}
 
